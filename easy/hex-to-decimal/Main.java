8f03e7c76432f0f2cced8d899dfa232fbc5873f4
Jonathan McCann <jonathan.mccann@liferay.com>
2014-09-05 11:34:51 -0700

    Hex to Decimal

---
+++ easy/hex-to-decimal/Main.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+    	File file = new File(args[0]);
+
+		BufferedReader in = new BufferedReader(new FileReader(file));
+
+		String line = null;
+
+		while ((line = in.readLine()) != null) {
+			System.out.println(Integer.parseInt(line, 16));
+		}
+	}
+}
