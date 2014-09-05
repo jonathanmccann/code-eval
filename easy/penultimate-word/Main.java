d6bf06e596261c194fbbe49494340ca1b2fcd2dd
Jonathan McCann <jonathan.mccann@liferay.com>
2014-09-05 11:07:38 -0700

    Penultimate Word

---
+++ easy/penultimate-word/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+    	File file = new File(args[0]);
+
+		BufferedReader in = new BufferedReader(new FileReader(file));
+
+		String line = null;
+
+		while ((line = in.readLine()) != null) {
+			String[] lineArray = line.split("\\s+");
+
+			if (lineArray.length > 0) {
+				System.out.println(lineArray[lineArray.length - 2]);
+			}
+		}
+	}
+}
