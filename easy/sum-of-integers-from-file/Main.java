6618c8d817fadeed02f511f52765f8138d197e2c
Jonathan McCann <jonathan.mccann@liferay.com>
2014-09-05 10:54:00 -0700

    Sum of Integers From File

---
+++ easy/sum-of-integers-from-file/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+    	File file = new File(args[0]);
+
+		BufferedReader in = new BufferedReader(new FileReader(file));
+
+		String line = null;
+
+		long sumTotal = 0;
+
+		while ((line = in.readLine()) != null) {
+			if (!line.isEmpty()) {
+				sumTotal = sumTotal + Integer.valueOf(line);
+			}
+		}
+
+		System.out.println(sumTotal);
+	}
+}
