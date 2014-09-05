ebfdafc457fe5f55e0ed75b5262d07eace203e1a
Jonathan McCann <jonathan.mccann@liferay.com>
2014-09-05 10:53:00 -0700

    Sum of Primes

---
+++ easy/sum-of-primes/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main (String[] args) {
+    	boolean isPrime = true;
+
+		long sumOfPrimes = 2;
+		long primeCandidate = 3;
+		long counter = 1;
+
+        while (counter < 1000) {
+			for (int i = 2; i <= (primeCandidate / 2); i++) {
+				if (primeCandidate % i == 0) {
+					isPrime = false;
+				}
+			}
+
+			if (isPrime) {
+				sumOfPrimes += primeCandidate;
+
+				counter++;
+			}
+
+			primeCandidate++;
+
+			isPrime = true;
+		}
+
+		System.out.println(sumOfPrimes);
+	}
+}
