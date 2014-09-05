bd76c7309b2e728e2613bf66ecfc7a9cafb24c28
Jonathan McCann <jonathan.mccann@liferay.com>
2014-09-05 11:37:46 -0700

    File Size

---
+++ easy/file-size/Main.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+    	File file = new File(args[0]);
+
+		System.out.println(file.length());
+	}
+}
