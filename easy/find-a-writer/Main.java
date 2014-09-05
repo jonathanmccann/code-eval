85b9eed2cbd13b26951d591f25a47eb1c00f1601
Jonathan McCann <jonathan.mccann@liferay.com>
2014-09-05 11:37:06 -0700

    Find a Writer - Increase Performance

--- easy/find-a-writer/Main.java
+++ easy/find-a-writer/Main.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Main {
     public static void main (String[] args) throws IOException {
@@ -16,8 +14,6 @@ public class Main {
 		while ((line = in.readLine()) != null) {
 			String[] lineArray = line.split("\\|");
 
-			StringBuilder sb = new StringBuilder();
-
 			if (lineArray.length > 0) {
 				char[] letters = lineArray[0].toCharArray();
 
@@ -26,10 +22,10 @@ public class Main {
 				for (String s : key) {
 					int keyValue = Integer.parseInt(s);
 
-					sb.append(letters[keyValue - 1]);
+					System.out.print(letters[keyValue - 1]);
 				}
 
-				System.out.println(sb.toString());
+				System.out.println();
 			}
 		}
 	}
