34d9f205eababa39f4b268ecb2af910f43282718
Jonathan McCann <jonathan.mccann@liferay.com>
2014-10-17 12:06:46 -0700

    Big Digits - Increase Performance

--- easy/big-digits/Main.java
+++ easy/big-digits/Main.java
@@ -3,10 +3,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -17,19 +14,17 @@ public class Main {
 		String line = null;
 
 		while ((line = in.readLine()) != null) {
-			List<Character> digits = new ArrayList<Character>();
+			List<Integer> digits = new ArrayList<Integer>();
 
-			for (int i = 0; i < line.length(); i++) {
-				Character c = line.charAt(i);
-
+			for (Character c : line.toCharArray()) {
 				if (Character.isDigit(c)) {
-					digits.add(c);
+					digits.add(Character.getNumericValue(c));
 				}
 			}
 
 			for (int i = 0; i < 6; i++) {
-				for (Character digit : digits) {
-					String[] text = digitToTextMap.get(digit);
+				for (int digit : digits) {
+					String[] text = digitToTextArray[digit];
 
 					System.out.print(text[i]);
 				}
@@ -39,62 +34,16 @@ public class Main {
 		}
 	}
 
-	private static Map<Character, String[]> digitToTextMap;
-
-	private static String[] _zero = {
-		"-**--", "*--*-", "*--*-", "*--*-", "-**--", "-----"
+	private static String[][] digitToTextArray = {
+		{"-**--", "*--*-", "*--*-", "*--*-", "-**--", "-----"},
+		{"--*--", "-**--", "--*--", "--*--", "-***-", "-----"},
+		{"***--", "---*-", "-**--", "*----", "****-", "-----"},
+		{"***--", "---*-", "-**--", "---*-", "***--", "-----"},
+		{"-*---", "*--*-", "****-", "---*-", "---*-", "-----"},
+		{"****-", "*----", "***--", "---*-", "***--", "-----"},
+		{"-**--", "*----", "***--", "*--*-", "-**--", "-----"},
+		{"****-", "---*-", "--*--", "-*---", "-*---", "-----"},
+		{"-**--", "*--*-", "-**--", "*--*-", "-**--", "-----"},
+		{"-**--", "*--*-", "-***-", "---*-", "-**--", "-----"},
 	};
-
-	private static String[] _one = {
-		"--*--", "-**--", "--*--", "--*--", "-***-", "-----"
-	};
-
-	private static String[] _two = {
-		"***--", "---*-", "-**--", "*----", "****-", "-----"
-	};
-
-	private static String[] _three = {
-		"***--", "---*-", "-**--", "---*-", "***--", "-----"
-	};
-
-	private static String[] _four = {
-		"-*---", "*--*-", "****-", "---*-", "---*-", "-----"
-	};
-
-	private static String[] _five = {
-		"****-", "*----", "***--", "---*-", "***--", "-----"
-	};
-
-	private static String[] _six = {
-		"-**--", "*----", "***--", "*--*-", "-**--", "-----"
-	};
-
-	private static String[] _seven = {
-		"****-", "---*-", "--*--", "-*---", "-*---", "-----"
-	};
-
-	private static String[] _eight = {
-		"-**--", "*--*-", "-**--", "*--*-", "-**--", "-----"
-	};
-
-	private static String[] _nine = {
-		"-**--", "*--*-", "-***-", "---*-", "-**--", "-----"
-	};
-
-	static {
-		Map<Character, String[]> myMap = new HashMap<Character, String[]>();
-
-		myMap.put('0', _zero);
-		myMap.put('1', _one);
-		myMap.put('2', _two);
-		myMap.put('3', _three);
-		myMap.put('4', _four);
-		myMap.put('5', _five);
-		myMap.put('6', _six);
-		myMap.put('7', _seven);
-		myMap.put('8', _eight);
-		myMap.put('9', _nine);
-
-		digitToTextMap = Collections.unmodifiableMap(myMap);
-	}
 }
