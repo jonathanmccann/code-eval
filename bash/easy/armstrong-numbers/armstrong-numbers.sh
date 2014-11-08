1cd0a4cb4a9b0477bb19acf4873038a21cb2eca6
Jonathan McCann <jonathan.mccann@liferay.com>
2014-11-07 16:07:12 -0800

    Armstrong Numbers - Bash

---
+++ bash/easy/armstrong-numbers/armstrong-numbers.sh
@@ -0,0 +1,18 @@
+while read -r LINE || [[ -n $LINE ]]; do
+    NUMBER_SIZE=${#LINE}
+
+    DIGITS=$(echo $LINE | sed -e 's/\(.\)/\1\n/g')
+
+    TOTAL_SUM=0
+
+    for DIGIT in $DIGITS ; do
+		TOTAL_SUM=`expr $TOTAL_SUM + $(($DIGIT**$NUMBER_SIZE))`
+	done
+
+	if [[ $TOTAL_SUM == $LINE ]]
+	then
+		echo "True"
+	else
+		echo "False"
+	fi
+done < $1
